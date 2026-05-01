c3fecab91c33a7623fd16c9b00fc51c43d273d0a
Mohammad J AL-Omran <mohammad.j.alomran@gmail.com>
2026-05-01 16:27:20 +0300

    first commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FieldTrainingApp.Controllers
+{
+    public class StudentController : Controller
+    {
+      
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            return View();
+        }
+    }
+}
